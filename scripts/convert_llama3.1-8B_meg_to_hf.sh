da097f04a5eba6c03625a267048df2157ed7c259
Risto Luukkonen <risto.m.luukkonen@gmail.com>
2025-03-31 16:23:10 +0300

    uncomment sbatch variables

--- scripts/convert_llama3.1-8B_meg_to_hf.sh
+++ scripts/convert_llama3.1-8B_meg_to_hf.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-##SBATCH --job-name=conv
-##SBATCH --nodes=1
-##SBATCH --mem=0
-##SBATCH --partition=dev-g
-##SBATCH --time=00-00:30:00
-##SBATCH --gpus-per-node=mi250:1
-##SBATCH --account=project_462000353
-##SBATCH --output=logs/%j.out
-##SBATCH --error=logs/%j.err
+#SBATCH --job-name=conv
+#SBATCH --nodes=1
+#SBATCH --mem=0
+#SBATCH --partition=dev-g
+#SBATCH --time=00-00:30:00
+#SBATCH --gpus-per-node=mi250:1
+#SBATCH --account=project_462000353
+#SBATCH --output=logs/%j.out
+#SBATCH --error=logs/%j.err
 
 CONTAINER=/appl/local/containers/sif-images/lumi-pytorch-rocm-6.2.4-python-3.12-pytorch-v2.6.0.sif
 export SINGULARITY_BIND=/pfs,/scratch,/projappl,/project,/flash,/appl,/usr/lib64/libjansson.so.4,/usr/lib64/libcxi.so.1,/opt/cray,/var/spool/slurmd
